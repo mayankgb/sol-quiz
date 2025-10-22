17cbc17bc126870bf1daf84322eb886d610e8492
Mayank Agrawal <mayankagrawal@Mayanks-MacBook-Air.local>
2025-10-22 18:26:35 +0530

    fixes

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from "../node_modules/.prisma/client"
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
