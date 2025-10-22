ee29ad2fb188c34ed66aac7d5120c94b0ebe8de2
Mayank Agrawal <mayankagrawal@Mayanks-MacBook-Air.local>
2025-10-22 16:58:11 +0530

    fixes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    transpilePackages: ['@repo/db'],
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', '@prisma/engines'],
-  },
-   outputFileTracingIncludes: {
-    '/api/**/*': ['../../packages/db/src/generated/prisma/**/*'],
-  },
-
-  // For standalone output
-  output: 'standalone',
 };
 export default nextConfig;
